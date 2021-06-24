b17cf4d1cb139f6db28715d8c75ca6a9d25d55a5
Douglas McGilvray <dm@studio-fas.io>
2021-06-24 12:24:45 -0300

    Update FacetFilter component to interact with search app

--- src/features/solang/components/SolangFacet/SolangFacet.tsx
+++ src/features/solang/components/SolangFacet/SolangFacet.tsx
@@ -1,31 +1,62 @@
-import { useAppSelector } from "../../../../app/hooks";
+import { useAppDispatch, useAppSelector } from "../../../../app/hooks";
 import { RootState } from "../../../../app/store";
-import { getAppFromState, getFacetCountsFromState, getFilterFromState } from "../../solang.slice";
+import { getAppFromState, getFacetCountsFromState, getFilterFromState, setParam } from "../../solang.slice";
 
 import './solang-facet.scss';
+import { ChangeEvent } from "react";
 
 interface MyProps {
+  appId: string
   alias: string
 }
 
-const SolangFacet = ({alias}: MyProps) => {
+const SolangFacet = ({appId, alias}: MyProps) => {
 
   const CLASS = 'solang-facet';
 
-  const filterState  = useAppSelector((state: RootState) => getFilterFromState(state.solang, 'searchApp', alias) );
+  const dispatch = useAppDispatch();
 
-  const facetCounts  = useAppSelector((state: RootState) => getFacetCountsFromState(state.solang, 'searchApp', alias) );
+  const filterState  = useAppSelector((state: RootState) => getFilterFromState(state.solang, appId, alias) );
+  const facetCounts  = useAppSelector((state: RootState) => getFacetCountsFromState(state.solang, appId, alias) )
+
+  const filterSelected: string[] = useAppSelector((state: RootState) => {
+    const app = getAppFromState(state.solang, appId);
+    return app ? app.params[alias] as [] || [] : [];
+  });
+
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+
+    let newState = [...filterSelected];
+    let value = e.target.value;
+
+    if (e.target.checked && !newState.includes(value)) {
+      newState.push(value);
+      dispatch(setParam({appId: appId, key: alias, value: newState}));
+    }
+    else if (!e.target.checked && newState.includes(value)) {
+      dispatch(setParam({appId: appId, key: alias, value: newState.filter(v => v !== value)}));
+    }
+
+  }
 
   return (
 
     <div className={`${CLASS}`}>
       <fieldset>
         {filterState.config.label && <legend>{filterState.config.label}</legend>}
+        <p>Selected: {filterSelected.join(', ')}</p>
+
         <ul className={`${CLASS}__list`}>
           { facetCounts.map( ({value, count}) => (
             <li className={`${CLASS}__list-item`}>
               <label  className={`${CLASS}__label`}>
-                <input type="checkbox" value={value} name={alias}/> <span className={`${CLASS}__value`}>{value}</span>  <span className={`${CLASS}__count`}>({count})</span>
+                <input
+                  type="checkbox"
+                  checked={filterSelected.includes(value)}
+                  onChange={changeHandler}
+                  value={value}
+                  name={alias}/> <span className={`${CLASS}__value`}>{value}</span>  <span className={`${CLASS}__count`}>({count})</span>
               </label>
             </li>
           ))}
